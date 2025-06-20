400799611ac8ec82f2c8e807caa2d1d94be36bec
Ruan <ruantmelon@icloud.com>
2025-06-20 04:37:21 -0300

    Module completed!

---
+++ data_types/script.js
@@ -0,0 +1,31 @@
+// Integers are represented by the int/number type
+let numberOfApples = 3;
+console.log('Counts are integers');
+
+/* Decimals are represented by the float type, but in JavaScript 
+they are represented by the number type as well */
+let myMoney = 145.6;
+console.log('Money can have decimal numbers');
+
+// Texts are represented by the string type
+let name = 'Ruan';
+console.log('Names are texts');
+
+// States can be represented by the boolean type
+let isMarried = false;
+console.log('Questions with yes or no answers are booleans');
+
+const x = 10;
+// typeof is used to return the type of a variable or constant
+let type = typeof(x);
+console.log(type);
+
+// null is a special value that represents the intentional absence of any object value.
+let bananasEaten = null;
+console.log('No bananas were eaten');
+
+// undefined is the total absence of a value
+let apples;
+console.log(`In this case, the \'apples\' could be a string like \'red\',
+a number like 3, or a boolean like false (meaning there aren\'t any apples)\n`
+,typeof apples);
